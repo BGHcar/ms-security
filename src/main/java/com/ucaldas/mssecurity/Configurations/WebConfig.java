fd9956f07aaadddc2106c969878d886c1143995e
BGHcar <bryan.1701720431@ucaldas.edu.co>
2024-05-11 13:28:24 -0500

    creacion de usuario como cliente

--- src/main/java/com/ucaldas/mssecurity/Configurations/WebConfig.java
+++ src/main/java/com/ucaldas/mssecurity/Configurations/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(securityInterceptor)
                 .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/public/**", "/api/users/public/**", "/api/users/{}/role/{roleId}");
+                .excludePathPatterns("/api/public/**", "/api/users/public/**");
 
     }
 }
